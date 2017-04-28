a97c8e49e478c9e4a5d10f44fef446137eb5cea4
Luiggi Vilca <lvilca@ebdperu.com>
2017-04-28 16:06:33 -0500

    Initial commit

---
+++ app/src/main/java/com/ebdperu/appebd/adapter/PictureAdapterRecyclerView.java
@@ -0,0 +1,96 @@
+package com.ebdperu.appebd.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.widget.RecyclerView;
+import android.transition.Explode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ebdperu.appebd.R;
+import com.ebdperu.appebd.model.Picture;
+import com.ebdperu.appebd.view.PictureDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lvilca on 21/04/2017.
+ */
+
+public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdapterRecyclerView.PictureViewHolder>{
+
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activity;
+
+    public PictureAdapterRecyclerView(ArrayList<Picture> pictures, int resource, Activity activity) {
+        this.pictures = pictures;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    @Override
+    public PictureViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Inflamos el View.
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource, parent, false);
+        return new PictureViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PictureViewHolder holder, int position) {
+        //Paso de Datos de cada Objeto
+        Picture picture = pictures.get(position); //Position, primer objeto se lo asignamos a este cardview.
+        holder.usernameCard.setText(picture.getUsername()); //Agregando los datos
+        holder.timeCard.setText(picture.getTime());
+        holder.likeNumberCard.setText(picture.getLike_number());
+        Picasso.with(activity).load(picture.getPicture()).into(holder.pictureCard);
+
+        holder.pictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, PictureDetailActivity.class);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    Explode explode = new Explode();
+                    explode.setDuration(1000);
+                    activity.getWindow().setExitTransition(explode);
+                    activity.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(activity, view, activity.getString(R.string.transitionname_picture)).toBundle());
+                }else{
+                    activity.startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        //Devolver un conteo de cuantos elementos tengo
+        return pictures.size();
+    }
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView pictureCard;
+        private TextView usernameCard;
+        private TextView timeCard;
+        private TextView likeNumberCard;
+
+        public PictureViewHolder(View itemView) {
+            super(itemView);
+
+            pictureCard = (ImageView) itemView.findViewById(R.id.pictureCard);
+            usernameCard = (TextView) itemView.findViewById(R.id.userNameCard);
+            timeCard = (TextView) itemView.findViewById(R.id.timeCard);
+            likeNumberCard = (TextView) itemView.findViewById(R.id.likeNumberCard);
+
+        }
+    }
+
+}
